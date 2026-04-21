81a6a05be502e7889112285787ddc390b7e08872
Guilherme Marques <mr.gmrsouza@gmail.com>
2026-04-21 11:40:50 -0300

    feat: implement logging system and express middleware setup
    Added Winston logger configuration
    
    Integrated Morgan, Helmet, and CORS
    
    Configured path aliases and Drizzle TS settings

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,23 @@
 import express from "express";
 import type { Request, Response } from "express";
+import logger from "#config/logger";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import cookiesParser from "cookie-parser";
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
+app.use(cookiesParser());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan("combined", { stream: { write: message => logger.info(message.trim()) } }));
 
 app.get("/", (req: Request, res: Response) => {
+  logger.info("Hello from AcquireFlow!");
   res.status(200).json({ message: "Hello from AcquireFlow!" });
 });
 
